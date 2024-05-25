2cb11971c062e84410a7a6cc83d56005964bf1f6
Francisco Pimenta <xisco.pimenta@gmail.com>
2024-05-25 22:06:16 +0100

    Restructuring file structure + clearing template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Banner'>
+        <h1>aDEVenture_</h1>
+      </div>      
+
+      <div className='Footer'>
+      <footer><a className='App-link' href="https://www.flaticon.com/free-icons/code" title="code icons">Icons</a> created by Royyan Wijaya - Flaticon</footer>
+      </div>
     </div>
   );
 }
